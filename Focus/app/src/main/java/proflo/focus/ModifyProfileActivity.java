cd8db3f7a37ca63b559e83597995ac9db61edd50
usman-f-sohail <usmanbssi@gmail.com>
2017-10-16 12:12:03 -0700

    started UI design of modify_activities, fixed the scroll ability for all the pages

--- Focus/app/src/main/java/proflo/focus/ModifyProfileActivity.java
+++ Focus/app/src/main/java/proflo/focus/ModifyProfileActivity.java
@@ -2,6 +2,11 @@ package proflo.focus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class ModifyProfileActivity extends AppCompatActivity {
 
@@ -9,5 +14,50 @@ public class ModifyProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_profile);
+
+        // set the toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // get the done button object
+        Button done = (Button)findViewById(R.id.done);
+        done.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v) {
+
+                // logic for creating profile here
+
+                // validate info
+                if(validate())
+                {
+                    // go back to the previous page
+
+                }
+
+            }
+        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // use the setToolbar method to handle this situation
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.modification_toolbar, menu);
+
+        return true;
+    }
+
+
+    boolean validate()
+    {
+        // implement validation here
+        return true;
+    }
+
 }
--- Focus/app/src/main/java/proflo/focus/ModifyScheduleActivity.java
+++ Focus/app/src/main/java/proflo/focus/ModifyScheduleActivity.java
@@ -2,6 +2,9 @@ package proflo.focus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ModifyScheduleActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class ModifyScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_schedule);
+
+        // set the toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // use the setToolbar method to handle this situation
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.modification_toolbar, menu);
+
+        return true;
     }
 }
