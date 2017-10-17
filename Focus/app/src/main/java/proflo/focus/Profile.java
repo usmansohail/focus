63f10863aea3fa14a66a4f10c23402e3395a76f7
Cameron Chilson <cchilson@usc.edu>
2017-10-16 23:33:38 -0700

    added code

---
+++ Focus/app/src/main/java/proflo/focus/Profile.java
@@ -0,0 +1,59 @@
+package proflo.focus;
+
+import java.util.Vector;
+
+/**
+ * Created by Cameron on 10/15/2017.
+ */
+
+public class Profile {
+
+    private String name;
+    private Vector<String> profileApps;
+    private boolean isActive;
+
+
+    public Profile(String name, Vector<String> profileApps, boolean isActive) {
+        this.name = name;
+        this.profileApps = profileApps;
+        this.isActive = isActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vector<String> getProfileApps() {
+        return profileApps;
+    }
+
+    public void setProfileApps(Vector<String> profileApps) {
+        this.profileApps = profileApps;
+    }
+
+    public boolean addAppToProfile(String appID){
+        profileApps.add(appID);
+        return true;
+    }
+
+    public boolean removeAppFromProfile(String appID){
+       return profileApps.remove(appID);
+    }
+
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void activate() {
+        isActive = true;
+    }
+
+    public void deactivate(){
+        isActive = false;
+    }
+}
